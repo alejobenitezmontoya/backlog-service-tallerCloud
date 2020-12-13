bf47674208321cc854e95f9f7d6672364b0b20d4
unknown <luis_benitez82103@elpoli.edu.co>
2020-12-13 15:27:11 -0500

    added data.sql and other configurations

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tbl_backlogs (id, project_identifier) VALUES (10,'ABC123');
+INSERT INTO tbl_backlogs (id, project_identifier) VALUES (20,'ABC456');
+INSERT INTO tbl_backlogs (id, project_identifier) VALUES (30,'ABC456');
